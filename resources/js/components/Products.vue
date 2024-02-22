cb747792d6035cda5cbb8569ff9561b5a344641e
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-02-22 18:34:54 -0300

    feat: add Laravel and Vue

---
+++ resources/js/components/Products.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        {{ name }}
+        <input type="text" name="name" v-model="name" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+</script>
