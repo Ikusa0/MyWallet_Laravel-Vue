94f70d0c83dc3bcb4d918e8865e8de8cee458933
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-02-23 00:27:08 -0300

    feat: add LoginPage view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import "./bootstrap";
 import { createApp } from "vue";
+import './presentation/styles/global.css'
 
-import App from "./components/App.vue";
+import LoginPage from "./presentation/pages/LoginPage.vue";
 
 const app = createApp();
 
-app.component("app", App);
+app.component("login", LoginPage);
 
 app.mount("#app");
---
+++ resources/js/presentation/pages/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="page">
+        <Logo />
+        <Form :data="formData" />
+        <a href="#">Primeira vez? Cadastre-se!</a>
+    </div>
+</template>
+
+<script setup>
+import Logo from "../components/Logo.vue";
+import Form from "../components/Form.vue";
+
+const formData = {
+    form: {
+        onSubmit: async (formValues) => {
+            console.log(formValues);
+        },
+    },
+    inputs: [
+        {
+            placeholder: "E-mail",
+            name: "email",
+            type: "email",
+            required: true,
+        },
+        {
+            placeholder: "Senha",
+            type: "password",
+            name: "password",
+            required: true,
+        },
+    ],
+    button: {
+        text: "Entrar",
+    },
+};
+</script>
