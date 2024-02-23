f3f19cdc17016462f19b5de188ed7789b4751179
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-02-23 00:32:21 -0300

    feat: add RegistrationPage view

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,11 @@ import { createApp } from "vue";
 import './presentation/styles/global.css'
 
 import LoginPage from "./presentation/pages/LoginPage.vue";
+import RegistrationPage from "./presentation/pages/RegistrationPage.vue";
 
 const app = createApp();
 
 app.component("login", LoginPage);
+app.component("registration", RegistrationPage);
 
 app.mount("#app");
---
+++ resources/js/presentation/pages/RegistrationPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="page">
+        <Logo />
+        <Form :data="formData" />
+        <a href="#">Já tem uma conta? Entre agora!</a>
+    </div>
+</template>
+
+<script setup>
+import Logo from "../components/Logo.vue";
+import Form from "../components/Form.vue";
+
+const formData = {
+    form: {
+        onSubmit: async (formValues) => {
+            console.log(formValues);
+        },
+    },
+    inputs: [
+        {
+            placeholder: "Nome",
+            name: "name",
+            required: true,
+        },
+        {
+            placeholder: "E-mail",
+            name: "email",
+            type: "email",
+            required: true,
+        },
+        {
+            placeholder: "Senha",
+            type: "password",
+            name: "password",
+            required: true,
+        },
+        {
+            placeholder: "Confirme a senha",
+            type: "password",
+            name: "passwordConfirmation",
+            required: true,
+        },
+    ],
+    button: {
+        text: "Cadastrar",
+    },
+};
+</script>
